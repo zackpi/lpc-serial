ef6e1255db03d6310c1ef000aea416b6204e24ca
Zachary Pitcher <zackpi@mit.edu>
2018-11-07 14:16:20 -0500

    2 spaces

--- __main__.py
+++ __main__.py
@@ -2,6 +2,6 @@ from lpc import Device
 
 
 if __name__ == "__main__":
-    microcontroller = Device("/dev/ttyUSB0", 9600, 50000)
-    microcontroller.sync()
-    print("hello world")
+  microcontroller = Device("/dev/ttyUSB0", 9600, 50000)
+  microcontroller.sync()
+  print("hello world")
